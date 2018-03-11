3eedd5aa1c9457d983a1bd7e08f74ca2e69fb5e2
mcmillanator <me@stanleymcmillan.com>
2018-03-11 15:56:00 -0400

    update install.sh
    update install.sh to handle hidden files
    this allows the .tmux.conf in the .tmux submodule to be symlinked correctly

--- install.sh
+++ install.sh
@@ -5,19 +5,26 @@ git submodule update
 DOTFILES=`pwd`
 echo "export DOTFILES=$DOTFILES" > ~/.dotfiles_config
 files=($(find . -name '*.symlink' -print))
-files+=(vim)
+files+=(vim .tmux/.tmux.conf)
 
 function link () {
 	ln -sfn $DOTFILES/$1 ~/.$2
+	echo "symlinked $1 to ~/.$2"
+}
+
+function parse-filename () {
+	local file=$(basename $1)
+	local file="${file#.}"
+	local file="${file%.symlink}"
+	echo $file
 }
 
 
 for i in "${files[@]}"
 do
   :
-	file=$(basename $i)
-	filename="${file%.*}"
-	link $i $filename
+	file=$(parse-filename $i)
+	link $i $file
 done
 
 source ~/.bashrc
