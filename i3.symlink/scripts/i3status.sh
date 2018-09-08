fb95ff88de38e77cc5099db70b4e7fc0779371e2
mcmillanator <me@stanleymcmillan.com>
2018-09-08 11:06:21 -0400

    update i3status.sh to get full name of the connected adapter

--- i3.symlink/scripts/i3status.sh
+++ i3.symlink/scripts/i3status.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # TODO: create loop for multiple interfaces
-adapter=`ifconfig -s | grep -v lo | awk '{if(NR>1) print $1}'`
+adapter=`ifconfig -s | grep -v lo | awk '{if($3>0) print $1}' | awk '{if(NR>1) print }'`
+adapter=`ifconfig | grep $adapter | awk '{print $1}' | sed 's/://g'`
 sed "s/\$interface/$adapter/g" ../ethernet.template > ../i3status.conf.symlink
 cat ../i3status.conf.template >> ../i3status.conf.symlink
