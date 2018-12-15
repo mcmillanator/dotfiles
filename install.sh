5a514fefaf8c1cb901085930855d45484251da79
mcmillanator <me@stanleymcmillan.com>
2018-12-15 10:16:47 -0500

    update install to work with config folder

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ DOTFILES=`pwd`
 $DOTFILES/i3.symlink/scripts/i3status.sh
 echo "export DOTFILES=$DOTFILES" > ~/.dotfiles_config
 files=($(find . -name '*.symlink' -print))
+config_files=($(ls config))
 files+=(vim .tmux/.tmux.conf)
 
 function link () {
-	ln -sfn $DOTFILES/$1 ~/.$2
-	echo "symlinked $1 to ~/.$2"
+	ln -sfn $DOTFILES/$1 $2
+	echo "symlinked $2 to $DOTFILES/$1"
 }
 
 function parse-filename () {
@@ -25,10 +26,16 @@ for i in "${files[@]}"
 do
   :
 	file=$(parse-filename $i)
-	link $i $file
+	link $i ~/.$file
 done
 
-ln -sfn $DOTFILES/config/ranger ~/.config
+
+for i in "${config_files[@]}"
+do
+  :
+	file=$(parse-filename $i)
+	link "~/.config/$i" "$file"
+done
 
 source ~/.bashrc
 i3-msg reload
