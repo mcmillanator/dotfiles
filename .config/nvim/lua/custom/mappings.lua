2eda8c171276125e761dbf9eae5ccdb7eaece630
mcmillanator <stan@stanleymcmillan.com>
2024-02-25 12:12:12 -0500

    Backup nvchad settings
    Stop backing up all of nvim since nvchad is a third party git repo
    Instead backup custom folder which has my personal configs

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -4,6 +4,18 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
   },
 }
 
