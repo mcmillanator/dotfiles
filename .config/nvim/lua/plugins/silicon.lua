8a9c5e95375fc73b4a08a295762a010e75b551e9
mcmillanator <stan@stanleymcmillan.com>
2024-03-05 22:16:25 -0500

    update silicon

--- .config/nvim/lua/plugins/silicon.lua
+++ .config/nvim/lua/plugins/silicon.lua
@@ -6,6 +6,9 @@ return {
     require("silicon").setup({
       font = "Hack",
       theme = "Solarized (dark)",
+      pad_horiz = 0,
+      pad_vert = 0,
+      no_round_corner = true,
       no_window_controls = true,
     })
   end,
