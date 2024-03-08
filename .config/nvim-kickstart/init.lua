c208b23d393d2892f72d78b390bd1238b6df328f
mcmillanator <stan@stanleymcmillan.com>
2024-03-08 14:26:12 -0500

    set term colors

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -3,6 +3,8 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 --]]
+vim.opt.termguicolors = true
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
