cea109e6252064ac16cd57da6b9acbd1df210397
mcmillanator <stan@stanleymcmillan.com>
2024-03-20 22:02:08 -0400

    force zsh shell in nvterm

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.o.shell = 'zsh'
 require("config.lazy")
--- .config/nvim/lua/plugins/nvterm.lua
+++ .config/nvim/lua/plugins/nvterm.lua
@@ -1,6 +1,8 @@
 return {
   "NvChad/nvterm",
   config = function()
-    require("nvterm").setup()
+    require("nvterm").setup({
+      shell = vim.o.shell,
+    })
   end,
 }
