5bfb2498eadbb0c1fc276cbf7e6c33da47bb6943
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2024-04-27 12:45:50 -0400

    refactor neotest config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.o.shell = "zsh"
 require("config.lazy")
--- I have not managed to get this neotest configuration to work anywhere else
-require("neotest").setup({
-  adapters = {
-    require("neotest-python")({
-      dap = { justMyCode = false },
-    }),
-  },
-})
