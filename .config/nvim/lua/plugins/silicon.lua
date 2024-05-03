531164321c5859d48321ab43bbcc7ac889b49432
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2024-05-02 23:57:08 -0400

    fix silicon config

--- .config/nvim/lua/plugins/silicon.lua
+++ .config/nvim/lua/plugins/silicon.lua
@@ -1,18 +1,15 @@
 return {
   "michaelrommel/nvim-silicon",
-  lazy = true,
+  lazy = false,
   cmd = "Silicon",
   config = function()
     require("silicon").setup({
-      font = "Hack",
+      font = "Hack Nerd Font",
       theme = "Solarized (dark)",
       pad_horiz = 0,
       pad_vert = 0,
       no_round_corner = true,
       no_window_controls = true,
-      watermark = {
-        text = " @mcmillanator",
-      },
     })
   end,
 }
