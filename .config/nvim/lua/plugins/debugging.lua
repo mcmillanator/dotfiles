764b6990587b24df90d5b8439ba93a1a67744ff6
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2024-07-09 09:41:25 -0400

    add nvim-dap-ruby

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     "rcarriga/nvim-dap-ui",
     "mfussenegger/nvim-dap-python",
+    "suketa/nvim-dap-ruby",
   },
   config = function()
     require("dapui").setup()
@@ -12,12 +13,6 @@ return {
     end
     dap.listeners.before.launch.dapui_config = function()
       dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
     end
   end,
 }
