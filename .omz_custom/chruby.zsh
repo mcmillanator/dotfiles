806ba078d65ca08a9b4732315ed00f29877bd328
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-02-12 15:25:12 -0500

    fix: detect chruby command and source files if command not found

--- .omz_custom/chruby.zsh
+++ .omz_custom/chruby.zsh
@@ -1,5 +1,6 @@
 # Source chruby
-if command -v chruby 2>&1 > /dev/null
+command -v chruby 2>&1 > /dev/null
+if [ $? -ne 0 ]
 then
   source /usr/local/share/chruby/chruby.sh
   source /usr/local/share/chruby/auto.sh
