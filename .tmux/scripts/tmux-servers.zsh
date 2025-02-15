995724d0efab002ccb6928763ffaa23774c29d27
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-02-14 21:40:49 -0500

    move tmux-servers script to .tmux/scripts

--- .tmux/scripts/tmux-servers.zsh
+++ .tmux/scripts/tmux-servers.zsh
@@ -1,11 +1,10 @@
 #!/usr/bin/zsh
-echo "Starting script"
 server=$(ls /tmp/tmux-1000 | fzf --tmux --prompt='Choose server: ')
-echo "Server choosen"
 if [[ -n "${TMUX}" ]]; then
   tmux detach-client -E "tmux -S /tmp/tmux-$(id -u)/$server attach-session"
+  exit
 else
   tmux -S /tmp/tmux-$(id -u)/$server attach-session
+  exit
 fi
-echo "Tmux server switched"
 
