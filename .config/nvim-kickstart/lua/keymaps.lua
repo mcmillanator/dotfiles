7c8ded87735ba4a73b22ecbef396193edea9df33
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-02-28 23:27:07 -0500

    lint

--- .config/nvim-kickstart/lua/keymaps.lua
+++ .config/nvim-kickstart/lua/keymaps.lua
@@ -1,129 +1,129 @@
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Variables
 local map = vim.keymap.set
-local term = require 'nvterm.terminal'
-local term_modes = { 'n', 'v', 'i', 't' }
+local term = require("nvterm.terminal")
+local term_modes = { "n", "v", "i", "t" }
 --TODO: move to dap config file
 --local dap = require 'dap'
 -- TODO: move to dapui config file
 --local dapui = require 'dapui'
 local term_float = function()
-  term.toggle 'float'
+  term.toggle("float")
 end
 local term_vertical = function()
-  term.toggle 'vertical'
+  term.toggle("vertical")
 end
 local term_horizontal = function()
-  term.toggle 'horizontal'
+  term.toggle("horizontal")
 end
 
 -- Which key prefix registration
-local wk = require 'which-key'
-wk.add {
-  { '<leader>c', group = 'Code' },
-  { '<leader>d', group = 'Document' },
-  { '<leader>g', group = 'Git' },
-  { '<leader>gc', group = 'Commit' },
-  { '<leader>r', group = 'Rename' },
-  { '<leader>s', group = 'Search' },
-  { '<leader>w', group = 'Workspace' },
+local wk = require("which-key")
+wk.add({
+  { "<leader>c", group = "Code" },
+  { "<leader>d", group = "Document" },
+  { "<leader>g", group = "Git" },
+  { "<leader>gc", group = "Commit" },
+  { "<leader>r", group = "Rename" },
+  { "<leader>s", group = "Search" },
+  { "<leader>w", group = "Workspace" },
   {
-    '<leader>b',
-    group = 'buffers',
+    "<leader>b",
+    group = "buffers",
     expand = function()
-      return require('which-key.extras').expand.buf()
+      return require("which-key.extras").expand.buf()
     end,
   },
-}
-wk.add {
-  { '<leader>t', group = 'Telescope/Terminal' },
-  { '<leader>gs', group = 'Gitsigns' },
-  { '<leader>d', group = 'Debugger' },
-}
+})
+wk.add({
+  { "<leader>t", group = "Telescope/Terminal" },
+  { "<leader>gs", group = "Gitsigns" },
+  { "<leader>d", group = "Debugger" },
+})
 
-map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Open Lazy UI' })
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy UI" })
 -- Define conficting keymaps
 -- Neotree
-map('n', '<leader>n', '<cmd>Neotree toggle<cr>', { desc = 'Neotree toggle' })
-map('n', '<leader>ge', '<cmd>Neotree git_status toggle<cr>', { desc = 'Git explorer' })
-map('n', '<leader>gf', '<cmd>Neotree git_status toggle reveal<cr>', { desc = 'Reveal in git explorer' })
-map('n', '<C-n>', '<cmd>Neotree toggle<cr>', { desc = 'Neotree toggle' })
-map('n', '<C-f>', '<cmd>Neotree reveal<cr>', { desc = 'Neotree reveal' })
+map("n", "<leader>n", "<cmd>Neotree toggle<cr>", { desc = "Neotree toggle" })
+map("n", "<leader>ge", "<cmd>Neotree git_status toggle<cr>", { desc = "Git explorer" })
+map("n", "<leader>gf", "<cmd>Neotree git_status toggle reveal<cr>", { desc = "Reveal in git explorer" })
+map("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Neotree toggle" })
+map("n", "<C-f>", "<cmd>Neotree reveal<cr>", { desc = "Neotree reveal" })
 -- Telescope keymaps
-map('n', '<leader>tn', '<cmd>Telescope notify<cr>', { desc = 'Telescope notify' })
-map('n', '<leader>tk', '<cmd>Telescope keymaps<cr>', { desc = 'Telescope keymaps' })
-map('n', '<leader>tk', '<cmd>Telescope keymaps<cr>', { desc = 'Telescope keymaps' })
-map('n', '<leader>gst', '<cmd>Telescope git_status<cr>', { desc = 'git status' })
-map('n', '<leader>gS', '<cmd>Telescope git_status<cr>', { desc = 'git status' })
+map("n", "<leader>tn", "<cmd>Telescope noice<cr>", { desc = "Show notifications in Telescope" })
+map("n", "<leader>tk", "<cmd>Telescope keymaps<cr>", { desc = "Telescope keymaps" })
+map("n", "<leader>tk", "<cmd>Telescope keymaps<cr>", { desc = "Telescope keymaps" })
+map("n", "<leader>gst", "<cmd>Telescope git_status<cr>", { desc = "git status" })
+map("n", "<leader>gS", "<cmd>Telescope git_status<cr>", { desc = "git status" })
 -- Search
-map('n', '<leader>sc', '<cmd>Telescope commands<cr>', { desc = 'Commands' })
-map('n', '<leader>sk', '<cmd>Telescope keymaps<cr>', { desc = 'Commands' })
+map("n", "<leader>sc", "<cmd>Telescope commands<cr>", { desc = "Commands" })
+map("n", "<leader>sk", "<cmd>Telescope keymaps<cr>", { desc = "Commands" })
 -- Find
-local telescope = require 'telescope.builtin'
+local telescope = require("telescope.builtin")
 
-map('n', '<leader>fp', function()
-  telescope.find_files { cwd = require('lazy.core.config').options.root }
-end, { desc = 'Find Plugin File' })
+map("n", "<leader>fp", function()
+  telescope.find_files({ cwd = require("lazy.core.config").options.root })
+end, { desc = "Find Plugin File" })
 
-map('n', '<leader>fa', function()
-  telescope.find_files { hidden = true, no_ignore = true, use_git_root = true }
-end, { desc = 'Find All Files (git root)' })
+map("n", "<leader>fa", function()
+  telescope.find_files({ hidden = true, no_ignore = true, use_git_root = true })
+end, { desc = "Find All Files (git root)" })
 
-map('n', '<leader>ff', function()
-  telescope.find_files { hidden = true, use_git_root = true }
-end, { desc = 'Find Files (git root)' })
+map("n", "<leader>ff", function()
+  telescope.find_files({ hidden = true, use_git_root = true })
+end, { desc = "Find Files (git root)" })
 
-map('n', '<C-p>', function()
-  telescope.find_files { hidden = true, use_git_root = true }
-end, { desc = 'Find Files (git root)' })
+map("n", "<C-p>", function()
+  telescope.find_files({ hidden = true, use_git_root = true })
+end, { desc = "Find Files (git root)" })
 
 -- Gitsigns keymaps
-map('n', '<leader>ga', '<cmd>Gitsigns stage_hunk<cr>', { desc = 'git add' })
-map('n', '<leader>gd', '<cmd>Gitsigns diffthis<cr>', { desc = 'git dff' })
-map('n', '<leader>gu', '<cmd>Gitsigns undo_stage_hunk<cr>', { desc = 'git unstage' })
-map('n', '<leader>gsd', '<cmd>Gitsigns toggle_deleted<cr>', { desc = 'Toggle Deleted' })
-map('n', '<leader>gsw', '<cmd>Gitsigns toggle_word_diff<cr>', { desc = 'Toggle Word Diff' })
-map('n', '<leader>gsn', '<cmd>Gitsigns toggle_numhl<cr>', { desc = 'Toggle Num Hightlight' })
-map('n', '<leader>gss', '<cmd>Gitsigns toggle_signs<cr>', { desc = 'Toggle Signs' })
-map('n', '<leader>gsh', '<cmd>Gitsigns toggle_linehl<cr>', { desc = 'Toggle Line Highlight' })
-map('n', '<leader>gsb', '<cmd>Gitsigns toggle_current_line_blame<cr>', { desc = 'Toggle Current Line Blame' })
+map("n", "<leader>ga", "<cmd>Gitsigns stage_hunk<cr>", { desc = "git add" })
+map("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", { desc = "git dff" })
+map("n", "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", { desc = "git unstage" })
+map("n", "<leader>gsd", "<cmd>Gitsigns toggle_deleted<cr>", { desc = "Toggle Deleted" })
+map("n", "<leader>gsw", "<cmd>Gitsigns toggle_word_diff<cr>", { desc = "Toggle Word Diff" })
+map("n", "<leader>gsn", "<cmd>Gitsigns toggle_numhl<cr>", { desc = "Toggle Num Hightlight" })
+map("n", "<leader>gss", "<cmd>Gitsigns toggle_signs<cr>", { desc = "Toggle Signs" })
+map("n", "<leader>gsh", "<cmd>Gitsigns toggle_linehl<cr>", { desc = "Toggle Line Highlight" })
+map("n", "<leader>gsb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle Current Line Blame" })
 -- nvterm keymaps
-map('n', '<leader>tf', term_float, { noremap = true, silent = true, desc = 'Terminal Floating' })
-map('n', '<leader>th', term_horizontal, { noremap = true, silent = true, desc = 'Terminal Horizontal' })
-map('n', '<leader>tv', term_vertical, { noremap = true, silent = true, desc = 'Terminal Vertical' })
+map("n", "<leader>tf", term_float, { noremap = true, silent = true, desc = "Terminal Floating" })
+map("n", "<leader>th", term_horizontal, { noremap = true, silent = true, desc = "Terminal Horizontal" })
+map("n", "<leader>tv", term_vertical, { noremap = true, silent = true, desc = "Terminal Vertical" })
 -- TODO: Move to dap config file
 -- nvim-dap
-map('n', '<Leader>dt', function()
+map("n", "<Leader>dt", function()
   dap.toggle_breakpoint()
-end, { desc = 'Toggle Debug Breakpoint' })
+end, { desc = "Toggle Debug Breakpoint" })
 
-map('n', '<Leader>dc', function()
+map("n", "<Leader>dc", function()
   dap.continue()
-end, { desc = 'Debug Continue' })
+end, { desc = "Debug Continue" })
 
-map('n', '<Leader>dbq', function()
+map("n", "<Leader>dbq", function()
   dap.list_breakpoints()
-end, { desc = 'Quickfix all breakpoints' })
+end, { desc = "Quickfix all breakpoints" })
 
-map('n', '<Leader>dbc', function()
+map("n", "<Leader>dbc", function()
   dap.clear_breakpoints()
-end, { desc = 'Clear all breakpoints' })
+end, { desc = "Clear all breakpoints" })
 
-map('n', '<Leader>di', function()
+map("n", "<Leader>di", function()
   dap.set_into()
-end, { desc = 'Step into' })
+end, { desc = "Step into" })
 
-map('n', '<Leader>do', function()
+map("n", "<Leader>do", function()
   dap.set_over()
-end, { desc = 'Set over' })
+end, { desc = "Set over" })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous Diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next Diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic Error messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous Diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next Diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic Error messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -131,25 +131,25 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
