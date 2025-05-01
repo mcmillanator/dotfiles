73d36b82de9e0de4fdb6e890942a75de2d5853fb
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-05-01 10:46:10 -0400

    Add aerial to nvim

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,30 @@
+-- A code outline window for skimming and quick navigation
+-- https://github.com/stevearc/aerial.nvim
+
+return {
+  "stevearc/aerial.nvim",
+  lazy = false,
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("aerial").setup({
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "<leader>a[", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "<leader>a]", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+      filter_kind = false,
+      manage_folds = true,
+      link_folds_to_tree = false,
+      link_tree_to_folds = true,
+      show_guides = true,
+      autojump = true,
+    })
+    vim.keymap.set("n", "<leader>at", "<cmd>AerialToggle!<CR>")
+  end,
+}
