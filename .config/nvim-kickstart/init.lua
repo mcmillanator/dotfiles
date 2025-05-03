68c31b8d451d76d3ac62979d27cfd2e2470e8ca3
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-05-03 14:24:54 +0000

    extract todo-comments from init.lua

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -284,8 +284,6 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
