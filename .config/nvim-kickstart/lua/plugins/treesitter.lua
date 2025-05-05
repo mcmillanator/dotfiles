68b84ca613618182dee44dd7079fa1e705aee100
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-05-05 09:59:52 -0400

    config: add keymaps for treesitter navigation

--- .config/nvim-kickstart/lua/plugins/treesitter.lua
+++ .config/nvim-kickstart/lua/plugins/treesitter.lua
@@ -28,6 +28,16 @@ return { -- Highlight, edit, and navigate code
       'xml',
       'yaml',
     },
+    incremental_selection = { enable = true },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+        goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+        goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+        goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+      },
+    },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
