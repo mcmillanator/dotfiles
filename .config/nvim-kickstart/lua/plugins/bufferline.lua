3f1f7120601992de017f29e0a0478492d6d17b65
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-05-05 12:00:42 -0400

    feat: add bufferline

---
+++ .config/nvim-kickstart/lua/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+-- A snazzy 💅 buffer line (with tabpage integration) for Neovim built using lua.
+vim.opt.termguicolors = true
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      diagnostics = 'nvim_lsp',
+      always_show_bufferline = false,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'Neo-tree',
+          highlight = 'Directory',
+          text_align = 'left',
+        },
+        {
+          filetype = 'snacks_layout_box',
+        },
+      },
+    },
+  },
+}
