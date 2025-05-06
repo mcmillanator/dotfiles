6fd5d19fb7a397df3d5463f366f3bbdf8d503396
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-05-06 13:25:21 -0400

    conf: set which-key window height

--- .config/nvim-kickstart/lua/plugins/which-key.lua
+++ .config/nvim-kickstart/lua/plugins/which-key.lua
@@ -2,6 +2,10 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   opts = {
+    win = {
+      -- width = 10,
+      height = { min = 5, max = 1000 },
+    },
     -- delay between pressing a key and opening which-key (milliseconds)
     -- this setting is independent of vim.opt.timeoutlen
     delay = 0,
