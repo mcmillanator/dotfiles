9e43e91911abbe1f81a1a87968d381c83ceb1f53
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-05-21 13:30:11 -0400

    config: add scroll up and down keys to autocompletion

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -57,6 +57,16 @@ return { -- Autocompletion
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'default',
+      ['<C-d>'] = {
+        function(cmp)
+          cmp.scroll_documentation_down()
+        end,
+      },
+      ['<C-u>'] = {
+        function(cmp)
+          cmp.scroll_documentation_up()
+        end,
+      },
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
