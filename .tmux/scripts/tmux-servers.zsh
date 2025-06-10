2f647753c1f0f61ba51460fa055f56a870ec4f62
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-06-10 19:45:19 -0400

    feat: test for existing tmux session before trying to attach

--- .tmux/scripts/tmux-servers.zsh
+++ .tmux/scripts/tmux-servers.zsh
@@ -1,10 +1,14 @@
 #!/usr/bin/zsh
-server=$(ls /tmp/tmux-1000 | fzf --tmux --prompt='Choose server: ')
-if [[ -n "${TMUX}" ]]; then
-  tmux detach-client -E "tmux -S /tmp/tmux-$(id -u)/$server attach-session"
-  exit
-else
-  tmux -S /tmp/tmux-$(id -u)/$server attach-session
-  exit
+socket_name=$(ls /tmp/tmux-1000 | fzf --tmux --prompt='Choose socket_name: ')
+# Check if tmux socket_name for socket (-L) is running by combining socket check and process check
+if [[ -S "/tmp/tmux-$(id -u)/$socket_name" ]] && tmux -S "/tmp/tmux-$(id -u)/$socket_name" list-sessions >/dev/null 2>&1; then
+  # if a tmux session is found
+  if [[ -n "${TMUX}" ]]; then
+    tmux detach-client -E "tmux -S /tmp/tmux-$(id -u)/$socket_name attach-session"
+    exit
+  else
+    tmux -S /tmp/tmux-$(id -u)/$socket_name attach-session
+    exit
+  fi
 fi
 
