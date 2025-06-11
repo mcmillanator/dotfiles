dca72b1b02cf5047a4b981628a75a7d1a632793e
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-06-11 14:09:44 -0400

    fix: check if source files exist for chruby before sourcing them

--- .omz_custom/chruby.zsh
+++ .omz_custom/chruby.zsh
@@ -1,8 +1,11 @@
 # Source chruby
 command -v chruby 2>&1 > /dev/null
-if [ $? -eq 0 ]
-then
-  source /usr/local/share/chruby/chruby.sh
-  source /usr/local/share/chruby/auto.sh
-  chruby ruby
+if [ $? != 0 ]
+  then
+  if [ -f '/usr/local/share/chruby/chruby.sh' ] && [ -f '/usr/local/share/chruby/auto.sh' ]
+    then
+    source /usr/local/share/chruby/chruby.sh
+    source /usr/local/share/chruby/auto.sh
+    chruby ruby
+  fi
 fi
