1c7ed1596562934a0f986b5b2004e2dfba9ed439
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-08-10 12:39:57 -0400

    feat: echo results on prebuild and prebuildpush

--- .omz_custom/aliases.zsh
+++ .omz_custom/aliases.zsh
@@ -80,17 +80,25 @@ dcbuild ()
 
 dcprebuildpush()
 {
+  local results=""
+
   for i in $(docker images | grep mcmillanator/prebuilt | awk '{print $1}' )
-  docker push $i
+  docker push $i && results="$results\n$i: Success" || results="$results\n$i: Failed"
+
+  echo $results
 }
 
 dcprebuild()
 {
-  dcbuild prebuilt/base mcmillanator/prebuilt-base:latest
-  dcbuild prebuilt/ts mcmillanator/prebuilt-ts:latest
-  dcbuild prebuilt/tf mcmillanator/prebuilt-tf:latest || true
-  dcbuild prebuilt/python mcmillanator/prebuilt-python:latest || true
-  dcbuild prebuilt/ruby mcmillanator/prebuilt-ruby:latest || true
+  local results=""
+  
+  dcbuild prebuilt/base mcmillanator/prebuilt-base:latest && results="$results\nprebuilt/base: Success" || results="$results\nprebuilt/base: Failed"
+  dcbuild prebuilt/ts mcmillanator/prebuilt-ts:latest && results="$results\nprebuilt/ts: Success" || results="$results\nprebuilt/ts: Failed"
+  dcbuild prebuilt/tf mcmillanator/prebuilt-tf:latest && results="$results\nprebuilt/tf: Success" || results="$results\nprebuilt/tf: Failed"
+  dcbuild prebuilt/python mcmillanator/prebuilt-python:latest && results="$results\nprebuilt/python: Success" || results="$results\nprebuilt/python: Failed"
+  dcbuild prebuilt/ruby mcmillanator/prebuilt-ruby:latest && results="$results\nprebuilt/ruby: Success" || results="$results\nprebuilt/ruby: Failed"
+  
+  echo -e "$results"
 }
 
 dcrebuild()
