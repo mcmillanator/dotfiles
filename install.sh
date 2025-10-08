e31a01e1d45b5450713f758471817ddfaa6dfa6f
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-10-08 16:22:29 -0400

    move and rename entrypoint.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 echo "Running entrypoint as user: ${USER}"
-mackup_dir=~/Documents/Projects/dotfiles/.mackup
-mackup_conf=~/Documents/Projects/dotfiles/.mackup.cfg
+mackup_dir=$(pwd)/.mackup
+mackup_conf=$(pwd)/.mackup.cfg
 if [ ! -d ~/.mackup ]; then
   echo "folder not found linking"
 	ln -fs $mackup_dir ~/.mackup
@@ -11,4 +11,4 @@ if [ ! -f ~/.mackup.cfg ]; then
   echo "file not found linking"
 	ln -fs $mackup_conf ~/.mackup.cfg
 fi
-/usr/local/py-utils/bin/mackup restore -f
+mackup restore -f
