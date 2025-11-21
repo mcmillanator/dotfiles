441e52233764baf3baede8841b37ab8e3d447f60
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2025-11-21 16:09:45 -0500

    add setup to todo-comments

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -5,6 +5,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = { signs = true },
   config = function()
+    require('todo-comments').setup()
     vim.keymap.set('n', ']t', function()
       require('todo-comments').jump_next()
     end, { desc = 'Next todo comment' })
