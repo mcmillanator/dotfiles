baf3b4827db5403edd620defdab2afdd56ad1912
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2026-03-16 13:10:29 -0400

    nit: move dotfiles to ~/.dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 echo "Running entrypoint as user: ${USER}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+dotfiles_repo="https://github.com/mcmillanator/dotfiles.git"
+git clone --depth 1 "${dotfiles_repo}" "${HOME}/.dotfiles"
 mackup_dir=$SCRIPT_DIR/.mackup
 mackup_conf=$SCRIPT_DIR/.mackup.cfg
 if [ ! -d ~/.mackup ]; then
