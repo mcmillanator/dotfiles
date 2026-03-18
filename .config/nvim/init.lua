131a2bef9b3f36e33d976ab7ce5019729f061f47
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2026-03-18 09:43:04 -0400

    refactor: let the user know lazy is being installed

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -219,6 +219,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  -- Inform the user so they don't interrupt
+  print 'Installing lazy.nvim, please wait...'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
