84248c039e57094b5fdecf313d2d0a1cde15768f
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2026-04-06 10:46:44 -0400

    feat: add hydra

---
+++ .config/nvim/lua/plugins/hydra.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    'nvimtools/hydra.nvim',
+    event = 'VeryLazy', -- Loads only when needed to keep startup fast
+    config = function()
+      local Hydra = require 'hydra'
+
+      Hydra {
+        name = 'Window Management',
+        mode = 'n',
+        body = '<C-w>',
+        config = {
+          -- 1000ms is the community favorite for a "sticky" mode
+          timeout = 1000,
+          invoke_on_body = true,
+          hint = {
+            border = 'rounded',
+            offset = 1,
+          },
+        },
+        heads = {
+          -- Navigation
+          { 'h', '<C-w>h' },
+          { 'j', '<C-w>j' },
+          { 'k', '<C-w>k' },
+          { 'l', '<C-w>l' },
+          -- Resizing (repeated taps feel very smooth here)
+          { '+', '2<C-w>+' },
+          { '-', '2<C-w>-' },
+          { '>', '5<C-w>>' },
+          { '<', '5<C-w><' },
+          -- Splits
+          { 's', '<C-w>s' },
+          { 'v', '<C-w>v' },
+          -- Exit
+          { 'q', '<C-w>q' },
+          { '<Esc>', nil, { exit = true } },
+        },
+      }
+    end,
+  },
+}
