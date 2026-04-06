90236a42736f907f7a1cc87b104ce3d0c035dea1
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2026-04-06 13:14:49 -0400

    chore: add dap-ui to edgy

--- .config/nvim/lua/plugins/edgy.lua
+++ .config/nvim/lua/plugins/edgy.lua
@@ -8,16 +8,44 @@ return {
       bottom = {
         { ft = 'toggleterm', title = 'Terminal', size = { height = 0.4 } },
         { ft = 'qf', title = 'Quickfix' },
+
+        {
+          title = 'DAP REPL',
+          ft = 'dap-repl',
+          size = { height = 0.3 },
+        },
+        {
+          title = 'DAP Console',
+          ft = 'dapui_console',
+          size = { height = 0.3 },
+        },
       },
       left = {
         'neo-tree',
+        {
+          title = 'DAP Scopes',
+          ft = 'dapui_scopes',
+          size = { width = 0.25 },
+        },
+        {
+          title = 'DAP Breakpoints',
+          ft = 'dapui_breakpoints',
+        },
+        {
+          title = 'DAP Stacks',
+          ft = 'dapui_stacks',
+        },
+        {
+          title = 'DAP Watches',
+          ft = 'dapui_watches',
+        },
         {
           title = 'Neo-Tree',
           ft = 'neo-tree',
           filter = function(buf)
             return vim.b[buf].neo_tree_source == 'filesystem'
           end,
-          size = { height = 0.5 },
+          -- size = { height = 0.5 },
         },
       },
       right = {
