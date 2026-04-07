9e7bea0c193771f98579b1973c0f6ab7197ef156
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2026-04-07 12:38:52 -0400

    conf: remove mini.icons

--- .config/nvim/lua/plugins/mini.icons.lua
+++ .config/nvim/lua/plugins/mini.icons.lua
@@ -1,2 +1,4 @@
 -- Icon provider
-return { 'echasnovski/mini.nvim', version = '*' }
+-- TODO: I don't think I need this. going to eval with it commented of for a while
+-- return { 'echasnovski/mini.nvim', version = '*' }
+return {}
