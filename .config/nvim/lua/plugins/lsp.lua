3fff82ce66adbf5a7cd4e78ab2ea7f8064033b37
mcmillanator <8310830+mcmillanator@users.noreply.github.com>
2026-04-07 12:39:04 -0400

    conf: add pyright

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -218,7 +218,7 @@ return {
         ruff = {},
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
